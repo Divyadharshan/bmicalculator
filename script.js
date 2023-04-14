d9bbb5e7ed4d80607342ce12d18a3366cb8ca68d
Divya Dharshan S <97381430+Divyadharshan@users.noreply.github.com>
2023-04-14 17:03:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ function bmi(){
     var a = document.getElementById("he").value;
     var b = document.getElementById("we").value;
 	if (a!="" && b!="" && a<=250 && b<=600){
-		var c = a/100;
+            var c = a/100;
 	    var bmi = b/c**2
 	    document.getElementById("display").innerHTML=bmi.toFixed(2);
 	}
-	else{
-		alert("KINDLY FILL ALL THE FIELDS or CHECK THE MARKS ENTERED");
-	}  
+   	else if (a=="" || b==""){
+            alert("Kindly fill all the fields");
+        }
+        else{
+            alert("Kindly Enter Appropriate Data");
+       }		 
 }
